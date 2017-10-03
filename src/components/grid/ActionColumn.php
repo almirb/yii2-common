783ef6556b50b781767b5a8cafa7ae440c1fb31f
Almir Bolduan <almir.bsi@gmail.com>
2017-10-03 09:28:01 -0300

    Update ActionColumn.php
    Widget was completelly rebuilt for better compatibility with Kartik's ActionColumn.

--- src/components/grid/ActionColumn.php
+++ src/components/grid/ActionColumn.php
@@ -7,87 +7,93 @@
  *
  * @author Germano Ricardi and Almir Bolduan
  *
- * @version 1.0
+ * @version 1.1
  *
  */
 
 namespace almirb\yii2common\components\grid;
 
-use yii;
-use yii\helpers\Html;
 use kartik\grid\ActionColumn as KartikActionColumn;
+use Yii;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Html;
 
 class ActionColumn extends KartikActionColumn
 {
+    const VIEW_BUTTON_DEFAULT_CLASS   = ['btn btn-sm btn-default'];
+    const UPDATE_BUTTON_DEFAULT_CLASS = ['btn btn-sm btn-primary'];
+    const DELETE_BUTTON_DEFAULT_CLASS = ['btn btn-sm btn-danger'];
+
+    /**
+     * @inheritdoc
+     */
     public $headerOptions	= ['class' => 'actions-buttons text-center'];
+
+    /**
+     * @inheritdoc
+     */
     public $contentOptions	= ['class' => 'text-center'];
+
+    /**
+     * @inheritdoc
+     */
     public $width = '130px';
 
-    public $viewButtonVisible   = true;
-    public $updateButtonVisible = true;
-    public $deleteButtonVisible = true;
+    /**
+     * @var boolean when the ActionColumn should avoid wrapping of buttons in more than one line
+     */
+    public $noButtonWrap = true;
 
     function init()
     {
-        parent::init();
         if (!$this->template) {
             $this->template = "<span class='btn btn-group btn-group-sm'>{view} {update} {delete}</span>";
         } else {
             $this->template = "<span class='btn btn-group btn-group-sm'>" . $this->template . "</span>";
         }
-        //Html::addCssStyle($this->headerOptions, "min-width:160px;");  TODO: Impedir que a ActionColumn quebre.
 
-        $this->initDefaultButtons();
+        $this->viewOptions     = array_replace_recursive(['class' => static::VIEW_BUTTON_DEFAULT_CLASS],   $this->viewOptions);
+        $this->updateOptions   = array_replace_recursive(['class' => static::UPDATE_BUTTON_DEFAULT_CLASS], $this->updateOptions);
+        $this->deleteOptions   = array_replace_recursive(['class' => static::DELETE_BUTTON_DEFAULT_CLASS], $this->deleteOptions);
+
+        if ($this->noButtonWrap) {
+            $width = 10 + (substr_count($this->template, '{') * 36);
+            $this->headerOptions = ['style' => "min-width: {$width}px"];
+        }
+
+        parent::init();
     }
 
-    public function run()
+    public function initDefaultButtons()
     {
-        return Html::decode($this->contentOptions);
-    }
+        if (!isset($this->buttons['delete']) && isset($this->deleteOptions['role']) && $this->deleteOptions['role'] == 'modal-remote') {
 
-    // protected
-    protected function initDefaultButtons(){
-
-        // view
-        if (($this->viewButtonVisible) && (!isset($this->buttons['view']))) {
-            $this->buttons['view'] = function ($url, $model, $key) {
-                $options = array_merge([
-                    'title' => Yii::t('app','View'),
-                    'aria-label' => Yii::t('app','View'),
+            $this->buttons['delete'] = function ($url) {
+                $options = $this->deleteOptions;
+                $title = Yii::t('kvgrid', 'Delete');
+                $icon = '<span class="glyphicon glyphicon-trash"></span>';
+                $label = ArrayHelper::remove($options, 'label', ($this->_isDropdown ? $icon . ' ' . $title : $icon));
+                $msg = ArrayHelper::remove($options, 'message', Yii::t('kvgrid', 'Are you sure to delete this item?'));
+                $options = array_replace_recursive([
+                    'aria-label' => $title,
                     'data-pjax' => '0',
-                    'class' => 'btn btn-sm btn-default',
-                ], $this->buttonOptions);
-                return Html::a('<span class="glyphicon glyphicon-eye-open"></span>', $url, $options);
+                    'data-method' => false,
+                    'data-request-method'=>'post',
+                    'data-confirm'=>false,
+                    'class' => static::DELETE_BUTTON_DEFAULT_CLASS,
+                    'data-confirm-title'=> Yii::t('kvdialog', 'Confirmation'),
+                    'data-confirm-message' =>  $msg,
+                ], $options);
+
+                if ($this->_isDropdown) {
+                    $options['tabindex'] = '-1';
+                    return '<li>' . Html::a($label, $url, $options) . '</li>' . PHP_EOL;
+                } else {
+                    return Html::a($label, $url, $options);
+                }
             };
         }
 
-        // update
-        if (($this->updateButtonVisible) && (!isset($this->buttons['update']))) {
-            $this->buttons['update'] = function ($url, $model, $key) {
-                $options = array_merge([
-                    'title' => Yii::t('app','Edit'),
-                    'aria-label' => Yii::t('app','Edit'),
-                    'data-pjax' => '0',
-                    'class' => 'btn btn-sm btn-primary',
-                ], $this->buttonOptions);
-                return Html::a('<span class="glyphicon glyphicon-pencil"></span>', $url, $options);
-            };
-        }
-
-        // delete
-        if (($this->deleteButtonVisible) && (!isset($this->buttons['delete']))) {
-            $this->buttons['delete'] = function ($url, $model, $key) {
-
-                $options = array_merge([
-                    'title' => Yii::t('app','Delete'),
-                    'aria-label' => Yii::t('app','Delete'),
-                    'data-confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
-                    'data-method' => 'post',
-                    'data-pjax' => '0',
-                    'class' => 'btn btn-sm btn-danger',
-                ], $this->buttonOptions);
-                return Html::a('<span class="glyphicon glyphicon-trash"></span>', $url, $options);
-            };
-        }
+        parent::initDefaultButtons();
     }
 }
